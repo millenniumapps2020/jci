8147d140e4076a6df94f79e22852fe66990a2bee
sudobrackets <sudobrackets@gmail.com>
2019-12-17 00:51:35 +0530

    push notification installed and firebase configured

--- src/views/Splash/SplashPage.js
+++ src/views/Splash/SplashPage.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Platform, StyleSheet, Animated, Text, View, Image, TouchableOpacity } from 'react-native';
 import { globalStyle, images } from '../../res';
 
+var PushNotification = require("react-native-push-notification");
+
 class SplashPage extends Component {
     state = {
         springValue: new Animated.Value(0.6),
@@ -11,7 +13,31 @@ class SplashPage extends Component {
         this.timerHandle = setTimeout(() => {
             this.props.navigation.navigate('Drawer');
         }, 3000);
+
+        this.pushNotificationTest()
     }
+
+    pushNotificationTest = () => {
+        PushNotification.configure({
+            onRegister: function (token) {
+                console.log("TOKEN:", token);
+            },
+
+            onNotification: function (notification) {
+                console.log("NOTIFICATION:", notification);
+                // notification.finish(PushNotificationIOS.FetchResult.NoData);
+            },
+            senderID: "318413914416",
+            permissions: {
+                alert: true,
+                badge: true,
+                sound: true
+            },
+            popInitialNotification: true,
+            requestPermissions: true
+        });
+    }
+
     spring() {
         Animated.spring(
             this.state.springValue,
